3a5b3a7916255757573fa8f3f5e45bc759292dfb
Vinicius <viniciussyn2000@gmail.com>
2021-05-25 10:47:33 -0300

    Correção de erros

---
+++ app/src/main/java/com/example/tela_de_login/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tela_de_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
